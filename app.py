bc7237d196689eb22eb0cc7bb37ac031eb8560aa
Yassine Ziti <yassine2ziti@gmail.com>
2022-07-08 23:51:41 +0100

    Styles Added

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import pickle
-import numpy as np
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
@@ -15,8 +14,7 @@ def predict():
         Year = int(request.form['Year'])
         Present_Price = float(request.form['Present_Price'])
         Kms_Driven = int(request.form['Kms_Driven'])
-        Kms_Driven2 = np.log(Kms_Driven)
-        Fuel_Type_Essence = request.form['Fuel_Type_Essence']
+        Fuel_Type_Essence = request.form['Fuel_Type']
         if(Fuel_Type_Essence == 'Essence'):
             Fuel_Type_Essence = 1
         else:
@@ -25,19 +23,19 @@ def predict():
         Year = 2020-Year
 
         	
-        Transmission_Manuelle = request.form['Transmission_Manuelle']
+        Transmission_Manuelle = request.form['Transmission_Type']
         if(Transmission_Manuelle == 'Manuelle'):
             Transmission_Manuelle = 1
         else:
             Transmission_Manuelle = 0
         prediction = model.predict([[Year,Kms_Driven,Present_Price,Transmission_Manuelle,Fuel_Type_Essence]])
-        output = round(prediction[0],2)
+        output = round(prediction[0])
         if output < 0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+            return render_template('index.html',prediction_text="You Can Sell The Car at",prediction = output)
     else:
-        return render_template('index.html')
+        return render_template('index.html',prediction_text='')
 
 if __name__ == "__main__":
     app.run(debug=True)
